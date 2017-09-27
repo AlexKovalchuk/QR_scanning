6de8df57ee65078e8941b56d97b1a46526be26c1
AlexKovalchuk <Songossj372@gmail.com>
2017-09-27 14:04:49 +0300

    show scaner on screen

--- app/components/ScanningPage.js
+++ app/components/ScanningPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, Linking} from 'react-native';
+import {View, Text, Linking, Alert, Dimensions, StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
@@ -10,22 +10,59 @@ class ScanningPage extends Component {
   }
 
   onSuccess(e) {
-    console.log('e=>', e);
+    console.log('e =>', e);
+    Alert.alert("QR Data: ", e.data);
     Linking.openURL(e.data).catch(err => console.error('An error occured', err));
   }
 
   render() {
+    const { centerText, textBold, buttonText, buttonTouchable } = styles;
+
     console.log('ScanningPage render Redux:', this.props.store);
     return(
-      <View>
-        <Text>
-          Here we will be scanning QR code and write it to the Redux!!!
-        </Text>
-        <QRCodeScanner onRead={this.onSuccess.bind(this)}/>
+      <View style={{flex: 1}}>
+        {/*<Text>*/}
+          {/*You can scan your QR-code.*/}
+        {/*</Text>*/}
+        <QRCodeScanner
+          title='Scan Code'
+          topContent={(
+            <Text style={centerText}>Top of the QR code scanner</Text>
+          )}
+          cameraStyle={{height: Dimensions.get("window").height}}
+          showMarker
+          bottomContent={(
+            <Text style={buttonText}>Scan the QR code</Text>
+          )}
+          onRead={this.onSuccess.bind(this)}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+
+  buttonTouchable: {
+    padding: 16,
+  },
+});
 
 const mapStateToProps = store => {
   return {
